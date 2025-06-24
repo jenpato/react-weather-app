df878ccebdb9894304cbae0fbe6023c7791ba2e8
jenpato <jenny.paterson1@gmail.com>
2025-06-24 12:59:39 +1000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Jen Paterson and is{" "}
-        <a
-          href="https://github.com/jenpato/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Jen Paterson and is{" "}
+          <a
+            href="https://github.com/jenpato/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
